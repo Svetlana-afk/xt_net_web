3a5fea4ab78ae06bc4bf704ad186a93f2deceebc
Svetlana Syakina <nikitinasd@gmail.com>
2020-05-25 20:18:13 +0400

    Task_1 1-8

--- Task_1/Task_1_1/Task_1_1_7_array_processing/Program.cs
+++ Task_1/Task_1_1/Task_1_1_7_array_processing/Program.cs
@@ -10,6 +10,53 @@ namespace Task_1_1_7_array_processing
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[15];
+            FillArray(arr);
+            Console.WriteLine("Сгенерированный случайным образом массив:");
+            PrintArray(arr);
+            SortArray(arr);
+            Console.WriteLine("Отсортированный массив:");
+            PrintArray(arr);
+            Console.WriteLine("Минимальное значение в Массиве: {0:D}", arr[0]);
+            Console.WriteLine("Максимальное значение в Массиве: {0:D}", arr[14]);
+
+        }
+        static void FillArray(int[] arr) 
+        {
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-20, 20);
+                
+            }
+
+        }
+        static void PrintArray(int[] arr)
+        {           
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                Console.WriteLine("{0:D}", arr[i]);
+            }
+        }
+        static void SortArray(int[] arr)
+        {
+            int a;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = 0; j < arr.Length-i-1; j++) 
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        a = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = a;
+                    }
+
+                }
+                
+            }
+
         }
     }
 }
---
+++ Task_1/Task_1_1/Task_1_1_8_no_positive/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_1_8_no_positive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] arr = new int[3,4,2];
+            FillArray(arr);
+            Console.WriteLine("Сгенерированный случайным образом трехмерный массив:");
+            PrintArray(arr);      
+            
+            Console.WriteLine("No Positive Массив:");
+            NoPositive(arr);
+            PrintArray(arr);
+        }
+
+        static void FillArray(int[,,] arr)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < arr.GetLength(0); ++i)
+            {
+                for (int j = 0; j < arr.GetLength(1); ++j)
+                {
+                    for (int k = 0; k < arr.GetLength(2); ++k)
+                    {
+                        arr[i, j, k] = rand.Next(-20, 20);
+                    }
+                }                  
+
+            }
+
+        }
+
+        static void PrintArray(int[,,] arr)
+        {            
+            for (int i = 0; i < arr.GetLength(0); ++i)
+            {
+                for (int j = 0; j < arr.GetLength(1); ++j)
+                {
+                    for (int k = 0; k < arr.GetLength(2); ++k)
+                    {
+                        Console.Write("{0}  ", arr[i, j, k]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
+
+        }
+
+        static void NoPositive(int[,,] arr)
+        {           
+            for (int i = 0; i < arr.GetLength(0); ++i)
+            {
+                for (int j = 0; j < arr.GetLength(1); ++j)
+                {
+                    for (int k = 0; k < arr.GetLength(2); ++k)
+                    {
+                        if (arr[i, j, k] > 0) { arr[i, j, k] = 0; }
+                         
+                    }
+                }
+
+            }
+
+        }
+
+    }
+}
