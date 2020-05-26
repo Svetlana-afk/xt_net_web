7ef97e0a660853a390285f7bba9e77e819661e4e
Svetlana Syakina <nikitinasd@gmail.com>
2020-05-26 21:12:00 +0400

    Tasks 1_2_1 +1_2_2

---
+++ Task_1/Task_1_2/Task_1_2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите небольшой рассказ о себе:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Очень интересно, средняя длина слова: {0:F}", AverageWordLength(str));    
+        }
+
+        //Функция AverageWordLength возвращает значение в виде числа с плавающей точкой
+        static float AverageWordLength(string str) 
+        {
+            int countOfWords = 0;
+            int sumOfСhars = 0;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '!', '?'};
+            string[] arrayOfWords = str.Split(delimiterChars);
+            foreach (var word in arrayOfWords)
+            {              
+                if (word.Length != 0) 
+                {
+                    countOfWords += 1;
+                    sumOfСhars += word.Length;               
+                }              
+            }
+            return (float)sumOfСhars / countOfWords;
+        }
+    }
+}
---
+++ Task_1/Task_1_2/Task_1_2_2_doubler/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2_2_doubler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напиши мне что-нибудь:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("И еще немножко):");
+            string str2 = Console.ReadLine();
+            Console.WriteLine("Дублирую символы: {0}", Doubler(str1,str2));
+        }
+
+        static StringBuilder Doubler(string str1, string str2)
+        {
+            StringBuilder doubler  = new StringBuilder("");
+            
+            foreach (var ch in str1)
+            {
+                if (str2.Contains(ch))
+                {
+                    doubler.Append(ch, 2);                    
+                }
+                else 
+                {
+                    doubler.Append(ch);
+                }
+            }
+            return doubler;
+        }
+    }
+}
