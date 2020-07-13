198d415aea13c04d8a8593a0501c596d100b8f02
Svetlana Syakina <nikitinasd@gmail.com>
2020-07-13 20:27:34 +0400

    Task 3_2_1 without *

---
+++ Task_3/Task_3_2_1_Dynamic_Array/Task_3_2_1_Dynamic_Array/DynamicArray.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_2_1_Dynamic_Array
+{
+    public class DynamicArray<T> : IEnumerable<T>, IEnumerable
+    {
+        private T[] Arr { get; set; }        
+        public int Capasity 
+        {   get => Arr.Length; 
+            set 
+            {
+                T[] tempArray = new T[value];
+                if (value >= Length) 
+                {
+                    Array.Copy(Arr, tempArray, Arr.Count());
+                    Arr = tempArray;
+                }               
+                else 
+                {
+                    Array.Copy(Arr, tempArray, value);
+                    Arr = tempArray;                   
+                    Length = value;
+                }
+            }
+        }
+        public int Length { get; private set; }
+        private const int  SIZE = 8;        
+
+        public DynamicArray()
+        {
+            Length = 0;
+            Arr = new T[SIZE];
+        }
+
+        public DynamicArray(int capasity) 
+        {
+            Length = 0;
+            Arr = new T[2*capasity];
+        }
+
+        public DynamicArray(ICollection<T> collection) 
+        {
+            Length = 0;
+            Arr = new T[2*collection.Count];  
+            int i = 0;
+            foreach (var item in collection)
+            {
+                Arr[i++] = item;
+                Length += 1;
+            } 
+        }
+        /// <summary>
+        /// Возвращает или задает элемент по указанному индексу.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T this[int index]
+        {
+            get
+            {
+                if (index <= Length)
+                {
+                    return Arr[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (index <= Length)
+                {
+                    Arr[index] = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        /// <summary>
+        /// Добавляет объект в конец коллекции
+        /// </summary>
+        /// <param name="item">Объект, добавляемый в конец коллекции</param>
+        public void Add(T item) 
+        {
+            if (Length >= Capasity) 
+            {
+                Capasity += Capasity;
+            }            
+            Arr[Length] = item;
+            Length++;  
+        }
+        /// <summary>
+        ///  Добавляет элементы указанной коллекции в конец списка
+        /// </summary>
+        /// <param name="collection">Коллекция, элементы которой добавляются в конец списка</param>               
+        public void AddRange(ICollection<T> collection) 
+        {
+            if (collection.Count + Length > Capasity)
+            {
+                Capasity += collection.Count;
+            }
+
+            foreach (var item in collection)
+            {
+                Arr[Length] = item;
+                Length++;
+            }
+        }
+        /// <summary>
+        /// Удаляет первое вхождение указанного объекта из коллекции
+        /// </summary>
+        /// <param name="item">Объект, который необходимо удалить из коллекции</param>
+        /// <returns></returns>
+        public bool Remove(T item) 
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                if (Arr[i].Equals(item)) 
+                {                    
+                    for (int j = i; j < Length-1; j++)
+                    {
+                        Arr[j] = Arr[j + 1];
+                    }
+                    Length--;
+                    return true;
+                }                
+            }
+            return false;
+        }
+        /// <summary>
+        /// Вставляет элементы коллекции в Dynamic_Array в позиции с указанным индексом.
+        /// </summary>
+        /// <param name="index"> Отсчитываемый от нуля индекс места вставки новых элементов. </param>
+        /// <param name="item"> Элемент, который следует вставить в коллекцию. </param>
+        public void Insert(int index, T item) 
+        {            
+            if (index < Length) 
+            {             
+                if (Length >= Capasity)
+                {
+                    Capasity += Capasity;                   
+                }
+                Length++;
+                for (int i = Length-1; i > index; i--)
+                {
+                    Arr[i] = Arr[i-1];
+                }
+                Arr[index] = item;
+            }
+        }        
+
+        public IEnumerator<T> GetEnumerator()
+        {            
+            return new DynamicArrayEnumerator(Arr, Length);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new DynamicArrayEnumerator(Arr, Length);
+        }
+        public class DynamicArrayEnumerator : IEnumerator, IEnumerator<T>
+        {
+            T[] dArray;
+            int length;
+            int position = -1;
+            public DynamicArrayEnumerator(T[] array, int length)
+            {
+                dArray = array;
+                this.length = length;
+            }
+            public bool MoveNext()
+            {
+                if (position == length-1)
+                {
+                    Reset();
+                    return false;
+                }
+                position++;
+                return true;
+            }
+            public void Reset()
+            {
+                position = -1;
+            }
+
+            void IDisposable.Dispose()
+            {
+               
+            }
+
+            public object Current
+            {
+                get
+                {
+                    if (position == -1 || position >= length)
+                        throw new InvalidOperationException();
+                    return dArray[position];
+                }
+            }
+
+            T IEnumerator<T>.Current => dArray[position];
+        }
+    }
+}
---
+++ Task_3/Task_3_2_1_Dynamic_Array/Task_3_2_1_Dynamic_Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_2_1_Dynamic_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                     
+            int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            List<int> b = new List<int>() { 10, 11, 12, 13, 14, 15, 16, 17 };
+            DynamicArray<int> darr = new DynamicArray<int>(a);
+            Console.WriteLine("DYNAMIC ARRAY");
+            printDynamicArray<int>(darr);
+            Console.WriteLine("Вывод массива после объединения с другой коллекцией и после вставки элемента:");
+            darr.AddRange(b);
+            darr.Insert(8, 9);            
+            printDynamicArray<int>(darr);
+            Console.WriteLine("Изменение емкости массива на  число меньшее, чем количество элементов.");
+            darr.Capasity = 11;
+            printDynamicArray<int>(darr);
+            Console.WriteLine("Удаление элемента:");
+            darr.Remove(5);
+            printDynamicArray<int>(darr);            
+            Console.WriteLine("\r\nВывод с помощью Итератора:");
+            IEnumerator<int> edarr = darr.GetEnumerator();
+            while (edarr.MoveNext())
+            {
+                int value = edarr.Current;
+                Console.Write(value + " ");               
+            }            
+        }
+        public static void printDynamicArray<T>(DynamicArray<T> darr) 
+        {
+            Console.WriteLine("Вывод массива: ");
+            foreach (var item in darr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\r\nДлина массива: {0}", darr.Length);
+            Console.WriteLine("Емкость массива: {0}", darr.Capasity);
+        }
+    }
+}
