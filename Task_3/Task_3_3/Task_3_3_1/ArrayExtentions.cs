ef9503f3b3ccda6aae677e23c03efda884485c61
Svetlana Syakina <nikitinasd@gmail.com>
2020-07-14 14:24:48 +0400

    Task_3_3_1

---
+++ Task_3/Task_3_3/Task_3_3_1/ArrayExtentions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_3_1
+{
+    public delegate int Function(int x);
+    
+    public static class ArrayExtentions
+    {        
+        public static void SuperArray(this int[] mass, Function function) 
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = function(mass[i]);
+            }
+        }
+        public static double FindAverage(this int[] mass) 
+        {
+            return mass.Average();
+        }
+
+        public static double FindSum(this int[] mass)
+        {
+            return mass.Sum();
+        }
+        public static int FindMostFrequent(this int[] mass) 
+        {
+            var mostFrequent = mass.GroupBy(elem => elem).OrderBy(elem => elem.Count()).Last();
+            return mostFrequent.Key;
+        }
+    }
+}
--- Task_3/Task_3_3/Task_3_3_1/Program.cs
+++ Task_3/Task_3_3/Task_3_3_1/Program.cs
@@ -10,6 +10,25 @@ namespace Task_3_3_1
     {
         static void Main(string[] args)
         {
+            int[] array = new int[]{ 1, 2, 3, 4, 5, 6, 7 , 8, 1};
+            array.SuperArray(MultiplyToTwo);
+            Console.WriteLine("МАссив после умножения его элементов на 2:");
+            PrintArray(array);
+            Console.WriteLine("\n\rСреднее значение массива:");
+            Console.WriteLine(array.FindAverage());
+            Console.WriteLine("\n\rСумма всех элементов массива:");
+            Console.WriteLine(array.FindSum());
+            Console.WriteLine("\n\rСамый часто встречающийся элемент массива:");
+            Console.WriteLine(array.FindMostFrequent());
+            
+        }
+        static int MultiplyToTwo(int A) => A * 2;
+        static void PrintArray(int[] array) 
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
