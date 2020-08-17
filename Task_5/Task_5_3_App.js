711c26a9db227ab3a1db82d54e82208ce709b78f
Svetlana Syakina <nikitinasd@gmail.com>
2020-08-17 16:58:01 +0400

    Task 5 Javascript DONE

--- Task_5/Task_5_3_App.js
+++
@@ -1,9 +0,0 @@
-import './Task_5_3_Mini_Crud.js';
-
-var storage = new Service();
-var id = storage.add({
-    name: "Sveta",
-    age: "ne skaju"
-});
-console.log(id);
-
--- Task_5/Task_5_3_Mini_Crud.js
+++ Task_5/Task_5_3_Mini_Crud.js
@@ -3,17 +3,18 @@ class Service {
     storage = [];
 
     add(obj) {
-        let id = Date.now().toString();
+
+        let id = (Date.now() + Math.random()).toString();
         let nObj = {
             id: id,
             obj: obj
         }
-        this.storage.push(nobj);
+        this.storage.push(nObj);
         return id;
     }
     getById(id) {
         for (let i = 0; i < this.storage.length; i++) {
-            if (this.storage[i] == id) {
+            if (this.storage[i].id == id) {
                 return this.storage[i].obj;
             }
         }
@@ -25,15 +26,14 @@ class Service {
         }
         return resultStorage;
     }
-    deleteById() {
-        let success = false;
+    deleteById(id) {
         for (let i = 0; i < this.storage.length; i++) {
             if (this.storage[i].id == id) {
                 this.storage.splice(i, 1);
-                success = true;
+                return true;
             }
         }
-        return success;
+        return false;
     }
     updateById(id, obj) {
         let success = false;
@@ -57,3 +57,37 @@ class Service {
         return success;
     }
 }
+
+var storage = new Service();
+var id1 = storage.add({
+    name: "Sveta",
+    age: "ne skaju",
+    hair: "blond"
+});
+
+var id2 = storage.add({
+    name: "Masha",
+    age: 35
+});
+
+var id3 = storage.add({
+    name: "Lena",
+    age: 35,
+    weight: 54
+});
+
+var katya = {
+    name : "Katya",
+    age : 15,
+    height : 175
+};
+console.log(storage.getById(id1));
+console.log(storage.getAll());
+console.log(storage.deleteById(id2));
+console.log(storage.getAll());
+storage.replaceById(id3, katya);
+console.log(storage.getAll());
+storage.updateById(id1, katya);
+console.log(storage.getAll());
+
+
